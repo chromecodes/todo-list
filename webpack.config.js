e2dd22ae536153d329ab8ce6e831e9b68f93824d
chromecodes <hameedhussain782@gmail.com>
2022-08-20 23:48:52 +0530

    updated files

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,18 @@ module.exports = {
 
     mode: 'development',
     entry: path.resolve(__dirname, 'src/js/index.js'),
-    ouput: {
-        path: path.resolve(__dirname, 'dist'),
+    devtool: 'inline-source-map',
+
+    output: {
         filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-}
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader'],
+            },
+        ],
+    },
+};
