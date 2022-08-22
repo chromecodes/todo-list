e65c4792e785a34fbe969fd4d7235ac1136e2daf
chromecodes <hameedhussain782@gmail.com>
2022-08-22 20:31:07 +0530

    updated files

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 import '../css/normalize.css'
 import '../css/style.css'
+
+import { todo } from './task'
+import { taskDisplay } from './display';
+
+const navTaskBtn = document.querySelector(".task-nav-btn");
+
+navTaskBtn.addEventListener('click', () => {
+    taskDisplay()
+})
+
+
