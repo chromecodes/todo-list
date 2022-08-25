0542ab3612c2c5a0190d70d4dcbb700e270204b6
chromecodes <hameedhussain782@gmail.com>
2022-08-25 16:17:20 +0530

    updated file

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ const navTaskBtn = document.querySelector(".task-nav");
 
 navTaskBtn.addEventListener('click', () => {
     taskDisplay();
+    todo.makeTodoList();
     const taskBtn = document.querySelector('.task-btn');
     const taskInputCnt = document.querySelector('.taskinputcnt');
     taskBtn.addEventListener('click', ()=> {
@@ -17,6 +18,8 @@ navTaskBtn.addEventListener('click', () => {
         const cancelBtn = document.querySelector('.cancel-btn');
             cancelBtn.addEventListener('click',taskCancel)
     })
+    const editBtns = document.querySelectorAll('.task-edit') 
+    console.log(editBtns);
 })
 
 function taskAdd(){
