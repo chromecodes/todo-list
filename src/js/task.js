de4ad48981fd78e6aaf879df026d8df48994d749
chromecodes <hameedhussain782@gmail.com>
2022-08-26 00:39:37 +0530

    added editing finctionality

--- src/js/task.js
+++ src/js/task.js
@@ -1,5 +1,5 @@
 export const todo = (() =>{
-    const todos = [{date: "2022-08-24" , priority: "low", task : "ca"},{date: "2022-08-24" , priority: "low", task : "ca"}];
+    const todos = [{date: "2022-08-24" , priority: "low", task : "ca"},{date: "2019-08-24" , priority: "high", task : "caaaaaaa"}];
 
     const createTodo = (task, date, priority) => {
         return ({task, date, priority})
@@ -10,8 +10,10 @@ export const todo = (() =>{
         console.log(todos);
     };
     const makeTodoList = () => {
+        console.log(todos);
 
         const temp = document.querySelector('.task-list-cnt')
+
 
             for (let i = 0; i < todos.length; i++) {
         
@@ -22,6 +24,7 @@ export const todo = (() =>{
                     taskCheck.setAttribute( 'class','task-check');
                     taskCheck.setAttribute( 'type','checkbox');
                     taskCheck.setAttribute( 'id',`task-check-${i}`);
+                    
                 taskElementCnt.appendChild(taskCheck);    
 
                     const taskElementCnt0 = document.createElement('div');
@@ -33,6 +36,13 @@ export const todo = (() =>{
                             const taskCheckLbl = document.createElement('label');
                             taskCheckLbl.setAttribute( 'class','task-check-lbl');
                             taskCheckLbl.setAttribute( 'for',`task-check-${i}`);
+                            if (todos[i].priority == 'low') {
+                                taskCheckLbl.style.color = 'var(--secondary-drgreen-accent)'
+                            } else if (todos[i].priority == 'high'){
+                                taskCheckLbl.style.color = 'var(--secondary-red-accent)'
+                            } else{
+                                taskCheckLbl.style.color = 'var(--secondary-blue-accent)'
+                            }
 
                         taskElementCnt1.appendChild(taskCheckLbl);    
 
@@ -55,12 +65,12 @@ export const todo = (() =>{
 
                             const taskEdit = document.createElement('button');
                             taskEdit.setAttribute( 'class','task-edit');
-                            taskEdit.setAttribute( 'index',`${i}`);
+                            taskEdit.setAttribute( 'data-index',`${i}`);
                         taskElementCnt2.appendChild(taskEdit);    
 
                             const taskDelete = document.createElement('button');
                             taskDelete.setAttribute( 'class','task-delete');
-                            taskDelete.setAttribute( 'index',`${i}`);
+                            taskDelete.setAttribute( 'data-index',`${i}`);
 
                         taskElementCnt2.appendChild(taskDelete);    
 
@@ -68,17 +78,94 @@ export const todo = (() =>{
                      
                 taskElementCnt.appendChild(taskElementCnt0);    
 
-
                     let tempHr = document.createElement("div");
                     tempHr.setAttribute("class","task-span");
 
                 taskElementCnt.appendChild(tempHr);    
 
-            temp.appendChild(taskElementCnt);    
+                    const taskUpdateCnt = document.createElement('div');
+                    taskUpdateCnt.setAttribute("class",`taskupdatecnt ${i}`);
+                        
+                        const taskUpdateCnt0 = document.createElement('div');
+                        taskUpdateCnt0.setAttribute("class","taskupdatecnt0");
+        
+                            const taskUpdateCnt1 = document.createElement('div');
+                            taskUpdateCnt1.setAttribute("class","taskupdatecnt1");
+        
+                                const taskUpdate = document.createElement('input');
+                                taskUpdate.setAttribute("type","text");
+                                taskUpdate.setAttribute("id","task-update");
+                                taskUpdate.value = todos[i].task;
+        
+                            taskUpdateCnt1.appendChild(taskUpdate);
+        
+                                const dateUpdate = document.createElement('input');
+                                dateUpdate.setAttribute("type","date");
+                                dateUpdate.setAttribute("id","date-update");
+                                dateUpdate.value = todos[i].date;
+
+                            taskUpdateCnt1.appendChild(dateUpdate);
+        
+                                const priorityUpdate = document.createElement('select');
+                                priorityUpdate.setAttribute("id","priority-update");
+        
+                                    const priorityUpdateOption1 = document.createElement('option');
+                                    priorityUpdateOption1.setAttribute("value","low");
+                                    priorityUpdateOption1.textContent = "Not at all important";
+                                    if(todos[i].priority == priorityUpdateOption1.value){
+                                    priorityUpdateOption1.setAttribute("selected","");
+                                    }
+        
+                                priorityUpdate.appendChild(priorityUpdateOption1);
+        
+                                    const priorityUpdateOption2 = document.createElement('option');
+                                    priorityUpdateOption2.setAttribute("value","mild");
+                                    priorityUpdateOption2.textContent = "It's some important ";
+                                    if(todos[i].priority == priorityUpdateOption2.value){
+                                        priorityUpdateOption2.setAttribute("selected","");
+                                    }
+        
+                                priorityUpdate.appendChild(priorityUpdateOption2);
+        
+                                    const priorityUpdateOption3 = document.createElement('option');
+                                    priorityUpdateOption3.setAttribute("value","high");
+                                    priorityUpdateOption3.textContent = "It's very important";
+                                    if(todos[i].priority == priorityUpdateOption3.value){
+                                        priorityUpdateOption3.setAttribute("selected","");
+                                    }
+        
+                                priorityUpdate.appendChild(priorityUpdateOption3);
+        
+                            taskUpdateCnt1.appendChild(priorityUpdate);
+        
+                        taskUpdateCnt0.appendChild(taskUpdateCnt1);
+        
+                            const taskUpdateCnt2 = document.createElement('div');
+                            taskUpdateCnt2.setAttribute("class","taskupdatecnt2");
+                            
+                                const taskAddBtn =  document.createElement('button');
+                                taskAddBtn.setAttribute("class","update-btn");
+                                taskAddBtn.setAttribute( 'data-index',`${i}`);
+                                taskAddBtn.textContent = "Update";
 
         
-        }
+                            taskUpdateCnt2.appendChild(taskAddBtn);
+        
+                                const taskCloseBtn =  document.createElement('button');
+                                taskCloseBtn.setAttribute("class","cancelupdate-btn");
+                                taskCloseBtn.setAttribute( 'data-index',`${i}`);
+                                taskCloseBtn.textContent = "Cancel";
+        
+                            taskUpdateCnt2.appendChild(taskCloseBtn);
+                            
+                        taskUpdateCnt0.appendChild(taskUpdateCnt2);
+        
+                    taskUpdateCnt.appendChild(taskUpdateCnt0);
+        
+                taskElementCnt.appendChild(taskUpdateCnt);
 
+            temp.appendChild(taskElementCnt);    
+        }
     }   
 
     return { todos, makeTodo, makeTodoList }
