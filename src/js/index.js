b477a7de1d08b82b72cbb8e7ccc9b2d09fb892e9
chromecodes <hameedhussain782@gmail.com>
2022-08-28 13:51:55 +0530

    updaated file

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,27 @@
 import '../css/normalize.css';
 import '../css/style.css';
 
-import { todo } from './object';
-import { taskDisplay } from './display';
+import { object } from './object';
+import { taskDisplay, projectsList } from './display';
 import { makeTodoList } from './maketodolist';
 import { listeners } from './listener';
 
 listeners.navListener()
+console.log( object);
+console.log( object.objects[0].name);
+console.log( object.objects[1].name);
+
+console.log( object.objects[0].obj[0].task);
+console.log( object.objects[0].obj[1].task);
+console.log( object.objects[0].obj[0].date);
+
+console.log( object.objects[1]);
+console.log( object.objects[1].obj[0].title);
+console.log( object.objects[1].obj[1].title);
+
+console.log( object.objects[1].obj[0].value[1].task);
+
+// console.log( object.objects[0][0].obj[0]);
+// console.log( object.objects[1]);
+
 
